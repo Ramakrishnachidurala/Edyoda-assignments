08d7f53d4bb6a83e8597a22812a9b87adc93e1d0
Ramakrishnachidurala <79012972+Ramakrishnachidurala@users.noreply.github.com>
2021-12-04 17:51:13 +0530

    Assignment-4 Square the elements of a list using map()

---
+++ squaresmap.py
@@ -0,0 +1,10 @@
+
+"""Python's map() is a built-in function that allows you to process and transform all the items in an iterable 
+   without using an explicit for loop, a technique commonly known as mapping. 
+   map() is useful when you need to apply a transformation function to each item in an iterable and transform them into a new iterable."""
+
+a=lambda i :i*3
+
+lis=[4,5,2,9]
+
+print(list(map(a,lis)))
